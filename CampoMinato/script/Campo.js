de460f3cb6dfff99e8360ca101b288045d749a55
Andra Rega <regandrea04@gmail.com>
2023-05-29 14:36:17 +0200

    Update Campo.js

--- CampoMinato/script/Campo.js
+++ CampoMinato/script/Campo.js
@@ -1,161 +1,161 @@
-class Campo { 
-    constructor(numRighe, numColonne, numMine) {
-        this.colonne = numColonne;
-        this.righe = numRighe;
-        this.numeroMine = numMine;
-        this.matriceCelle = [];
-        this.mineTrovate = 0;
-        this.isGiocoInCorso = true;
-        this.bandierine = 0;
-        this.aperte = 0;
-        this.totCelle = this.righe * this.colonne;
-    }
-    generaRandom(max) {
-        return Math.floor(Math.random() * max);
+class Campo {
+    constructor(grand, min) {
+        this.n = grand;
+        this.mine = min;
+        this.campo = [];
+        this.cAperte = 0;
+        this.gioco = true;
+        this.mineScoperte=0;
+        this.contatoreMine=min;
+
     }
 
-    ricomincia() {
+    reset(){
         location.reload();
     }
 
-    eliminaTuttoHtml(){
-        $("#celle").html("");
+    creaCampo() {
+        $('#numMine').text("mine: " + this.mine);
+        //crea la tabella
+        $('#griglia').css("height", $('#griglia').width() + "px");
+
+        for (let r = 0; r < this.n; r++) {
+            this.campo[r] = [];
+            for (let c = 0; c < this.n; c++) {
+                this.campo[r][c] = new Cella();
+                $('#griglia').append('<div class="cella" data-row="' + r + '" data-col="' + c + '">&nbsp;</div>');
+            }
+            //$('#griglia').append("<br>");
+        }
+        $('.cella').css("line-height", $('.cella').width() + "px");
+        $('.cella').css("font-size", (32 / 50) * $('.cella').width() + "px");
+
     }
 
+    //mette le mine a caso
+    posizionaMine() {
 
-    //genera posizione casuale mine
-    generaEPosizionaMine() {
-        let c, r;
-        for (let i = 0; i < this.numeroMine; i++) {
-            c = this.generaRandom(this.colonne);
-            r = this.generaRandom(this.righe);
-            //se la cella con coordinate r (riga) e c (colonne) è gia una cella si deve rifare
-            while (this.matriceCelle[r][c].cellaIsMina == true) {
-                c = this.generaRandom(this.colonne);
-                r = this.generaRandom(this.righe);
+        //mette le mine a caso
+        let m = 0;
+        while (m < this.mine) {
+            let pr = Math.round((this.n - 1) * Math.random());
+            let pc = Math.round((this.n - 1) * Math.random());
+            if (!this.campo[pr][pc].mina) {
+                this.campo[pr][pc].mina = true;
+                for (let i = -1; i <= 1; i++)
+                    for (let j = -1; j <= 1; j++) {
+                        let dr = pr + i;
+                        let dc = pc + j;
+                        if (dr >= 0 && dc >= 0 && dr < this.n && dc < this.n) {
+                            this.campo[dr][dc].cont++;
+                        }
+                    }
+                m++;
             }
-            this.matriceCelle[r][c].cellaIsMina = true;
         }
     }
 
+    //controlla se c'è una mina sulla casella scelta ed eventualmente su quelle vicine
+    controlla(r, c) {
+
+        if (this.campo[r][c].aperta)
+            return true;
+
+            if(  this.campo[r][c].flag==true){
+                this.campo[r][c].flag=false;
+                $('.cella[data-row=' + r + '][data-col=' + c + ']').removeClass("bandiera");
+                 this.contatoreMine++;
+                 this.aggiornaDisplayMine();
+            }
+        this.campo[r][c].aperta = true;
+        this.cAperte++;
+
+        $('.cella[data-row=' + r + '][data-col=' + c + ']').html(this.campo[r][c].cont ? this.campo[r][c].cont : "&nbsp;");
+        
+        $('.cella[data-row=' + r + '][data-col=' + c + ']').addClass("aperta");
+
+      
+        if (!this.campo[r][c].mina && !this.campo[r][c].cont) {
+
+            //controlla le zone vicino
+            for(var dr=r-1;dr<=r+1;dr++)
+            for(var dc=c-1;dc<=c+1;dc++)
+                if(dc >= 0 && dr >= 0 && dc < this.n && dr < this.n)
+                    this.controlla(dr,dc);
+
+        } else if (this.campo[r][c].mina) {
+            // qui mettiamo la mina
+            $('.cella[data-row=' + r + '][data-col=' + c + ']').html('<i class="fa fa-bomb"></i>');
+            $('.cella[data-row=' + r + '][data-col=' + c + ']').addClass("mina");
+            return false;
+        }
+
+        return true;
+
+    }
+//serve per far vedere tutte le mine una volta che si ha perso
+    scopriMine() {
+        for (let r = 0; r < this.n; r++)
+            for (let c = 0; c < this.n; c++)
+                if (this.campo[r][c].mina) {
+                    this.campo[r][c].aperta = true;
+                    if(this.campo[r][c].flag==true){
+                        $('.cella[data-row=' + r + '][data-col=' + c + ']').removeClass("bandiera");
+                    }
+                    $('.cella[data-row=' + r + '][data-col=' + c + ']').html('<i class="fa fa-bomb"></i>');
+                    $('.cella[data-row=' + r + '][data-col=' + c + ']').addClass("mina");
+                }
+    }
+
+
+
+
+    flag(r, c){
+    
+        if(this.gioco==true){
+            if (this.campo[r][c].aperta)
+            return true;
+    
+    
+         
+
+            this.campo[r][c].flag =!this.campo[r][c].flag;
     
 
-    creaCampo() {
-        let str = "";
-        $("#mine").text("mine presenti: " + this.numeroMine);
-
-        for(let i = 0; i < this.colonne; i++){
-            str += "auto ";
-        }
-        $(".container-playground").css("grid-template-columns", str);
-
-        for (let i = 0; i < this.righe; i++) {
-            this.matriceCelle[i] = [];
-            this.addRigaCelle(i);
-        }
-        this.generaEPosizionaMine();
-    }
-
-
-    addRigaCelle(numR) {
-        for (let c = 0; c < this.colonne; c++) {
-            this.matriceCelle[numR][c] = new myCella();
-            $("#celle").append("<div class='celleChiuse' data-row=" + numR + " data-coloumn=" + c + "></div>");
-        }
-    }
-
-    visualizzaMine() {
-        for (let r = 0; r < this.righe; r++){
-            for (let c = 0; c < this.colonne; c++){
-                if (this.matriceCelle[r][c].cellaIsMina) {
-                    this.matriceCelle[r][c].stato = "aperta";
-                    $('.celleChiuse[data-row=' + r + '][data-coloumn=' + c + ']').addClass("cellaMina");
-                }
+            if(this.contatoreMine>0){
+                if(  this.campo[r][c].flag==true){
+                    $('.cella[data-row=' + r + '][data-col=' + c + ']').addClass("bandiera");
+                    this.contatoreMine--;
+                    
+                    
             }
-        }
-    }
+            if(this.campo[r][c].mina && this.campo[r][c].flag){
+                this.mineScoperte++;
+            }
+            
 
-    mettiBandierina(rCella, cCella) {
-        if (this.isGiocoInCorso) {
-            //se hai preso una cella chiusa 
-            if (this.matriceCelle[rCella][cCella].stato == "chiusa"){
-                $('.celleChiuse[data-row=' + rCella + '][data-coloumn=' + cCella + ']').addClass("celleBandierina");
-                this.bandierine++;
-                this.matriceCelle[rCella][cCella].stato = "flag";
-                if(this.numeroMine - this.bandierine < 0){
-                    alert("numero bandierine superiore delle bombe!!");
-                    return 0;
-                }
-                $("#mine").text("mine presenti: " + (this.numeroMine - this.bandierine));
+
+
+            }
+            if(  this.campo[r][c].flag==false){
+                $('.cella[data-row=' + r + '][data-col=' + c + ']').removeClass("bandiera");
+                this.contatoreMine++;
+                   this.mineScoperte--;
             }
 
-            //se cella con bandierina torna ad essere cella chiusa
-            else if (this.matriceCelle[rCella][cCella].stato == "flag"){
-                $('.celleChiuse[data-row=' + rCella + '][data-coloumn=' + cCella + ']').removeClass("celleBandierina");
-                $('.celleChiuse[data-row=' + rCella + '][data-coloumn=' + cCella + ']').addClass("cellaChiusa");
-                this.matriceCelle[rCella][cCella].stato = "chiusa";
-                //se hai preso un mina
-                if (this.matriceCelle[rCella][cCella].cellaIsMina == true){
-                    this.mineTrovate--;
-                }
-                this.bandierine--;
-                this.aggiornaNumeroMineNelCampo();
+            if(this.mine==this.mineScoperte){
+                $('#stato').text("Hai vinto!");
+                alert("Hai Vinto!");
+                this.scopriMine();
+                this.gioco=false;
             }
-            if(this.mineTrovate == this.numeroMine){
-                this.visualizzaMine();
-                $("#feedback").text("hai vinto!!");
-                this.isGiocoInCorso = false;
-            }   
+            this.aggiornaDisplayMine();
+
+           
         }
-        else
-            alert("gioco finito");
-    }
-
-    controlloClick(rigaCella, colonnaCella) {
-        if (this.isGiocoInCorso) {
-
-            //cella aperta
-            if (this.matriceCelle[rigaCella][colonnaCella].stato == "aperta")
-                return 0;
-
-            //mina
-            if (this.matriceCelle[rigaCella][colonnaCella].cellaIsMina == true) {
-                $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').addClass("cellaMina");
-                $("#feedback").text("hai perso");
-                this.isGiocoInCorso = false;
-                this.visualizzaMine();
-                return 0;
-            }
-
-            //cella normale
-            $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').removeClass("celleBandierina");
-            $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').addClass("cellaBianca");
-            if(this.matriceCelle[rigaCella][colonnaCella].numMineVicine != 0)
-                $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').html(this.matriceCelle[rigaCella][colonnaCella].numMineVicine);
-
-            this.matriceCelle[rigaCella][colonnaCella].stato = "aperta";
-
-            this.aperte++;
-
-            //vuota
-            if (this.matriceCelle[rigaCella][colonnaCella].numMineVicine == 0) {
-                $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').removeClass("celleBandierina");
-                $('.celleChiuse[data-row=' + rigaCella + '][data-coloumn=' + colonnaCella + ']').addClass("cellaBianca");
-                for(var contR = rigaCella-1; contR <=rigaCella + 1; contR++){
-                    for (let contC = colonnaCella-1; contC <= colonnaCella+1; contC++) {
-                        if (contR >= 0 && contR < this.righe && contC >= 0 && contC < this.colonne)
-                            this.controlloClick(contR, contC);
-                    }
-                }
-            }
-
-            if(this.aperte == this.totCelle - this.numeroMine){
-                $("#feedback").text("hai vinto!!");
-                this.visualizzaMine();
-                this.isGiocoInCorso = false;
-            }
         }
-        else
-            alert("gioco finito");
-    }
+        
+     aggiornaDisplayMine(){
+        $('#numMine').text("mine: "+this.contatoreMine);
+     }
 }
